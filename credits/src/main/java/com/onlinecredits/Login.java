7b3c24668c8fdd5b4a24fb644b77a10855b36709
ShivaniGupta24 <shivanigupta0424@gmail.com>
2019-09-19 21:32:23 +0530

    Committing changes to run the application

--- credits/src/main/java/com/onlinecredits/Login.java
+++ credits/src/main/java/com/onlinecredits/Login.java
@@ -1,8 +1,18 @@
 package com.onlinecredits;
 
 public class Login {
+
     private int userName;
     private String password;
+
+    public int getUserName() {
+        return userName;
+    }
+
+    public void setUserName(int userName) {
+        this.userName = userName;
+    }
+
 
     public String getPassword() {
         return password;
--- credits/src/main/java/com/onlinecredits/LoginController.java
+++ credits/src/main/java/com/onlinecredits/LoginController.java
@@ -1,7 +1,6 @@
 package com.onlinecredits;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/login")
@@ -11,6 +10,21 @@ private final String sharedKey="SHARED_KEY";
 private static final String SUCCESS_MESSAGE="success";
 private static final String ERROR_MESSAGE="error";
 private static final int CODE_SUCCESS=100;
+private static final int AUTH_FAILURE=102;
+
+@RequestMapping(value="/hello", method = RequestMethod.POST)
+    public Response log(@RequestParam(value="name") String name, @RequestBody Login login) {
+    Response response = new Response();
+
+    int userName = login.getUserName();
+    String password= login.getPassword();
+
+    System.out.println(login.toString());
+
+    response.setName(name);
+    return response;
+}
+
 
 
 }
